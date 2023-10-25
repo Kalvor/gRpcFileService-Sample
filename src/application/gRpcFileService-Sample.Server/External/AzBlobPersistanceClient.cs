ab4e083aab93962c8c8d435ca1e60c57fc167bd6
Krzysztof <krzysztofjadczak55@gmail.com>
2023-10-25 21:46:50 +0200

    Renamed applications folder

---
+++ src/application/gRpcFileService-Sample.Server/External/AzBlobPersistanceClient.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using gRpcFileServiceSample.Server.Services;
+
+namespace gRpcFileServiceSample.Server.External
+{
+    internal sealed class AzBlobPersistanceClient : IFilePersistanceClient
+    {
+        public async Task UploadFileAsync(string filePath, Stream fileStream, CancellationToken cancellationToken = default)
+        {
+            var blobServiceClient = initializeBlobServiceClient();
+            var containerClient = blobServiceClient.GetBlobContainerClient("files");
+            if(! (await containerClient.ExistsAsync(cancellationToken)))
+            {
+                throw new Exception("Jebac komune");
+            }
+            var uploadResult = await containerClient.UploadBlobAsync(filePath, fileStream, cancellationToken);
+            //handle upres
+        }
+
+        private BlobServiceClient initializeBlobServiceClient()
+        {
+            return new BlobServiceClient(
+                new Uri("https://<storage-account-name>.blob.core.windows.net"),
+                new DefaultAzureCredential());
+        }
+    }
+}
---
+++ src/application/gRpcFileService-Sample.Server/Services/FileUploader.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using gRpcFileServiceSample.Server.Protos;
+
+namespace gRpcFileServiceSample.Server.Services
+{
+    public sealed class FileUploader : FileUploaderService.FileUploaderServiceBase
+    {
+        private readonly IFilePersistanceClient _FilePersistanceClient;
+        public FileUploader(IFilePersistanceClient filePersistanceClient)
+        {
+            _FilePersistanceClient = filePersistanceClient;
+        }
+
+        public override async Task<FileUploadResponse> UploadFile(IAsyncStreamReader<FileUploadRequest> requestStream, ServerCallContext context)
+        {
+            using Stream dataStream = new MemoryStream();
+            while (await requestStream.MoveNext())
+            {
+                dataStream.Write(requestStream.Current.Chunk.Span);
+            }
+            await _FilePersistanceClient.UploadFileAsync(Guid.NewGuid().ToString(), dataStream, context.CancellationToken);
+
+            return null;
+        }
+    }
+}
